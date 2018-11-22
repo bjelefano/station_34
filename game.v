1be91e60676fdbe643b8c03b0b7522c00c35f852
elefanob <brandon.elefano@mail.utoronto.ca>
2018-11-22 02:25:31 -0500

    Added String generator controller

--- game.v
+++ game.v
@@ -1,3 +1,146 @@
+module DisplayModule(string,go,clock,reset,out);
+	input [63:0] string;
+	input go;
+	input clock;
+	input reset;
+	
+	output reg [2:0] out;
+	
+	wire clk,bit;
+	
+	OutputRegister MSBit(string,go,clock,reset,bit);
+	RateDivider twoHz(26'b01011111010111100001000000 - 1'b1,clock,reset_clk,clk);
+	
+	reg [3:0] current_state, next_state;
+	
+	localparam WAIT = 4'd0,
+				  ONE = 4'd1,
+				  TWO = 4'd2,
+				  THREE = 4'd3,
+				  FOUR = 4'd4,
+				  OUT_TOGGLE = 4'd5,
+				  OUT_PUSH = 4'd6,
+				  OUT_MIC = 4'd7,
+				  OUT_MOUSE = 4'd8;
+	
+	always @(posedge clk, reset)
+   begin: state_FFs
+		if(~reset)
+			current_state <= WAIT;
+      else
+         current_state <= next_state;
+   end
+	
+	always @(*)
+   begin: state_table
+		case(current_state)
+			WAIT : next_state = go ? ONE : WAIT;
+			ONE : next_state = bit ? TWO : OUT_TOGGLE;
+			TWO : next_state = bit ? THREE : OUT_PUSH;
+			THREE : next_state = bit ? FOUR : OUT_MIC;
+			FOUR : next_state = OUT_MOUSE;
+			OUT_TOGGLE : next_state = bit ? ONE : WAIT;
+			OUT_PUSH : next_state = bit ? ONE : WAIT;
+			OUT_MIC : next_state = bit ? ONE : WAIT;
+			OUT_MOUSE : next_state = bit ? ONE : WAIT;
+		endcase
+   end
+	
+	always @(*)
+   begin: enable_signals
+		out = 3'd0;
+		case(current_state)
+			WAIT : out = 3'd0
+			OUT_TOGGLE : out = 3'd1;
+			OUT_PUSH : out = 3'd2
+			OUT_MIC : out = 3'd3
+			OUT_MOUSE : out = 3'd4
+		endcase
+   end
+	
+endmodule
+
+module OutputRegister(in,start,clock,reset,out);
+	input [63:0] in;
+	input start;
+	input clock;
+	input reset;
+	output reg out;
+	
+	reg [63:0] val1,val2;
+	
+	always @(posedge clock)
+	begin
+		if (reset == 1'b0)
+			begin
+				val1 <= 64'd0;
+				reset_clk <= 1'd0;
+			end
+		else if (start == 1'b0)
+			val1 <= in;
+		else if (val1[64] != 1'b1)
+				val1 <= val1 << 1'b1;
+		else
+			reset_clk <= 1'd1;
+	end
+	
+	reg clk,reset_clk;
+	RateDivider twoHz(26'b01011111010111100001000000 - 1'b1,clock,reset_clk,clk);
+
+	always @(posedge clk)
+	begin
+		if (reset == 1'b0)
+			begin
+				out <= 0;
+				val2 <= 64'd0;
+			end
+		else if (val1[64] == 1'b1)
+			val2 <= val1;
+		else
+			begin
+				out <= val2[64];
+				val2 <= val2 << 1'b1;
+			end
+	end
+	
+endmodule
+
+module StringGenerator(inc,clock,reset,out);
+	input inc;
+	input clock;
+	input reset;
+	
+	output [63:0] out;
+	
+	reg [2:0] counter;
+	reg [3:0] in;
+	
+	always @(posedge inc, reset)
+	begin
+		if(~reset)
+			counter <= 3'b000;
+		else if(inc)
+			counter <= (counter != 3'd4) ? counter + 1'b1 : 3'b001;
+	end
+	
+	always @(counter, reset)
+	begin
+		if (counter == 2'b00 | ~reset)
+			in <= 4'b0000;
+		else if (counter == 3'b001)
+			in <= 4'b0001;
+		else if (counter == 3'b010)
+			in <= 4'b0010;
+		else if (counter == 3'b011)
+			in <= 4'b0100;
+		else if (counter == 3'b100)
+			in <= 4'b1000;
+	end
+	
+	InputModule generator(in[0],in[1],in[2],in[3],clock,reset,out);
+	
+endmodule
+
 module InputModule(toggle,push,mic,mouse,clock,reset,out);
 	input toggle;
 	input push;
